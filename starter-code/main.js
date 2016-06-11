e7ea2b148392d4fa7401fb5c4192b1ab9b93f5a6
Natalia Polishchuk <Natalia@Natalias-MacBook-Pro.local>
2016-06-11 13:59:18 -0700

    created alert

--- starter-code/main.js
+++ starter-code/main.js
@@ -2,3 +2,12 @@ var cardOne = "queen";
 var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "king";
+if (cardTree===cardFour) {
+	alert("You found a match!";)
+}
+else if (cardOne===cardTwo) {
+	alert("You found a match!";)
+}
+else {
+	alert("Sorry, try again.";)
+}
