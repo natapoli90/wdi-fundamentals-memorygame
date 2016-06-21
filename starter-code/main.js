7a8e88b983c48242d3f16b229a1e9277c358c7b6
Natalia Polishchuk <Natalia@Natalias-MacBook-Pro.local>
2016-06-20 19:12:55 -0700

    better logic with Arrays

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,24 +1,41 @@
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
-
-if (cardTree===cardFour) {
-	alert("You found a match!";)
-}
-else if (cardOne===cardTwo) {
-	alert("You found a match!";)
-}
-else {
-	alert("Sorry, try again.";)
-}
+var cards = ['queen','queen','king','king'];
+var cardsInPlay = [];
 
 var board = document.getElementById('game-board');
 function createBoard() {
   for (var i=0; i<cards.length; i++) {
     var cardElement = document.createElement('div');
     cardElement.className = 'card';
+    cardElement.setAttribute('data-card', cards[i]);
+    cardElement.addEventListener('click', isTwoCards);
+	  board.appendChild(cardElement);
     board.appendChild(cardElement);
   }
 
 }
+function isTwoCards() {
+  cardsInPlay.push(this.getAttribute('data-card'));
+
+console.log(this.getAttribute('data-card'));
+	if (this.getAttribute('data-card') === 'king') {
+		this.innerHTML = "<img src='file:///Users/Natalia/fundamentals/Clubs%2013.png'>"; 
+	} else {
+		this.innerHTML = "<img src='file:///Users/Natalia/fundamentals/Clubs%2012.png'>"; 
+}
+  if (cardsInPlay.length === 2) {
+    isMatch(cardsInPlay);
+    cardsInPlay = [];
+  }
+
+}
+
+
+function isMatch(cards) {
+  if (cards[0] === cards[1]) {
+    alert("You found a match!");
+  } else {
+    alert("Sorry, try again.");
+  }
+}
+
+createBoard();
